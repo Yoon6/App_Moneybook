cd88949fe76dc861d1c06696963bcb306b1e692c
yoon_test <yoonda5898@gmail.com>
2019-09-25 17:48:39 +0900

    main페이지 수정 1. UI변경 2. write, setting 값넘기기 3. 초기화 어떻게 할지 생각하기

--- app/src/main/java/com/example/doit/wapproject2_test1/WriteFragment.java
+++ app/src/main/java/com/example/doit/wapproject2_test1/WriteFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class WriteFragment extends Fragment {
 
@@ -14,6 +16,20 @@ public class WriteFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_write,container,false);
+        /*
+        View v=inflater.inflate(R.layout.fragment_write,container,false);
+        return v;
+         */
+        View v = inflater.inflate(R.layout.fragment_write, container, false);
+
+        String [] values =
+                {"식비", "생활", "패션", "쇼핑", "교통", "문화여가", "교육학습", "여행", "선물", "유흥", "의료건강", "카페", "미용", "통신"};
+        Spinner spinner = (Spinner) v.findViewById(R.id.spinner_year);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+        return v;
     }
+
 }
