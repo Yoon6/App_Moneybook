549ead4479aa40a74c9ec1467d390199acb185d9
yoon <yoonda5898@gmail.com>
2020-01-30 17:31:19 +0900

    WritePage 편의성 수정
    카테고리항목 간소화
    확인, 취소시 초기화
    취소 경고창

--- app/src/main/java/com/example/doit/wapproject2_test1/WriteFragment.java
+++ app/src/main/java/com/example/doit/wapproject2_test1/WriteFragment.java
@@ -1,11 +1,14 @@
 package com.example.doit.wapproject2_test1;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +26,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class WriteFragment extends Fragment {
+    Spinner writeCategoryList;
 
     Button submitBtn;
     Button cancelBtn;
@@ -43,7 +47,23 @@ public class WriteFragment extends Fragment {
     private WriteValueSetListener writeValueSetListener;
 
 
+    private void fg_refresh() {
+        writeRadioButton1.setChecked(true);
+        setDate();
+        writePlace.setText(null);
+        writeCost.setText(null);
+        writeCategoryList.setSelection(0);
+    }
 
+    private void setDate(){
+        // 날짜는 현재 날짜로 고정
+        // 현재 시간 구하기
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        // 출력될 포맷 설정
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
+        writeDate.setText(simpleDateFormat.format(date));
+    }
     public interface CustomOnClickListener {
         void onSubmitClicked (View v);
     }
@@ -59,9 +79,10 @@ public class WriteFragment extends Fragment {
         final View v = inflater.inflate(R.layout.fragment_write, container, false);
 
         final String [] values =
-                {"식비", "생활", "패션", "쇼핑", "교통", "문화여가", "교육학습", "여행", "선물", "유흥", "의료건강", "카페", "미용", "통신"};
-        final Spinner writeCategoryList = v.findViewById(R.id.writeCategory);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
+                {"식비/생활", "패션/미용", "교통/통신", "문화/여가", "교육/학습", "의료/건강"};
+        writeCategoryList = v.findViewById(R.id.writeCategory);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(),
+                android.R.layout.simple_spinner_item, values);
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         writeCategoryList.setAdapter(adapter);
 
@@ -71,14 +92,7 @@ public class WriteFragment extends Fragment {
         writeRadioButton1=v.findViewById(R.id.writeRadioButton1);
         writeRadioButton2=v.findViewById(R.id.writeRadioButton2);
 
-        // 날짜는 현재 날짜로 고정
-        // 현재 시간 구하기
-        long now = System.currentTimeMillis();
-        Date date = new Date(now);
-        // 출력될 포맷 설정
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy년MM월dd일");
-        writeDate.setText(simpleDateFormat.format(date));
-
+        setDate();
         submitBtn = v.findViewById(R.id.submitBtn);
         cancelBtn = v.findViewById(R.id.cancelBtn);
         submitBtn.setOnClickListener(new View.OnClickListener() {
@@ -94,13 +108,12 @@ public class WriteFragment extends Fragment {
                 paramCost = writeCost.getText().toString();
                 paramPlace = writePlace.getText().toString();
                 paramDate = writeDate.getText().toString();
-                if(paramCost.isEmpty()){  // 가격을 기입하지 않을 경우 리스트에 요소 추가하지 않음
+                if(paramCost.isEmpty() || Integer.parseInt(paramCost)==0){  // 가격을 기입하지 않을 경우 리스트에 요소 추가하지 않음
                     Toast.makeText(getActivity().getApplicationContext(),"양식을 완성해주세요.",Toast.LENGTH_LONG).show();
                 }
                 else {
                     buttonClicked(v,paramCategory,paramCost,paramPlace,paramDate,paramState);
-                    writeCost.setText(null);
-                    writePlace.setText(null);
+                    fg_refresh();
                     Toast.makeText(getActivity().getApplicationContext(),"입력되었습니다.",Toast.LENGTH_LONG).show();
 
                 }
@@ -108,28 +121,10 @@ public class WriteFragment extends Fragment {
             }
         } );
 
-/*
-        submitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //submit 버튼 입력
-                Intent intent = new Intent(WriteFragment.this.getActivity() , MainFragment.class);
-                intent.putExtra("place",writePlace.getText().toString());
-                startActivity(intent);
-            }
-        });
-*/
-
-        final String [] texts = {"Place", "Cost"};
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*cancel 버튼 입력*/
-                for (int i = 0; i < 2; i++) {
-                    int resID = getResources().getIdentifier("write"+texts[i],
-                            "id", "com.example.doit.wapproject2_test1");
-                    ((EditText) v.findViewById(resID)).setText("");
-                }
+                cancelMsg();
             }
         });
 
@@ -153,5 +148,27 @@ public class WriteFragment extends Fragment {
         super.onAttach(context);
         writeValueSetListener = (WriteValueSetListener)context;
         customOnClickListener = (CustomOnClickListener)context;
+    }
+
+    public void cancelMsg() {
+        AlertDialog.Builder alert_confirm = new AlertDialog.Builder(getActivity());
+        alert_confirm.setMessage("작성내용이 삭제됩니다.").setCancelable(false).setPositiveButton("확인",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 'YES'
+                        fg_refresh();
+                    }
+                }).setNegativeButton("취소",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 'No'
+                        return;
+                    }
+                });
+        AlertDialog alert = alert_confirm.create();
+        alert.show();
+
     }
 }
